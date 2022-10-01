044b05d6c921a5aaf0d2601b04d8869d53fe8ccb
Niraj Kumar <64235951+im-niraj@users.noreply.github.com>
2022-10-01 07:53:14 +0530

    Iterative Approach

--- LeetCode_JAVA/509. Fibonacci Number.java
+++ LeetCode_JAVA/509. Fibonacci Number.java
@@ -1,4 +1,4 @@
-// Recursion Approch
+// Recursion Approach
 class Solution {
     public int fib(int n) {
         // if n is negative then return 0
@@ -22,3 +22,28 @@ class Solution {
 // Time Complexity: Exponential, O(2^n)
 // Space Complexity: O(1)
 // if we consider the function call stack size then O(n)
+
+
+
+
+// Iterative Approach
+class Solution {
+    public int fib(int n) {
+        int [] f= new int[n+2];
+        
+        f[0] = 0;
+        f[1] = 1;
+        
+        
+        for(int i=2; i<=n; i++){
+            f[i] = f[i-1] + f[i-2];
+        }
+        
+        return f[n];
+     
+    }
+}
+
+// Time Complexity: O(n)
+// Auxiliary space: O(n)
+
