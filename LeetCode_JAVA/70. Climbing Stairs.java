211929be72beb1b5a4ba77ca5a2f242023e6e1ea
Niraj Kumar <64235951+im-niraj@users.noreply.github.com>
2022-10-01 07:33:47 +0530

    Recursion method

--- LeetCode_JAVA/70. Climbing Stairs.java
+++ LeetCode_JAVA/70. Climbing Stairs.java
@@ -1,3 +1,5 @@
+// Approch 1. fibonacci
+
 class Solution {
     public int climbStairs(int n) {
        
@@ -23,4 +25,31 @@ class Solution {
 *  Like 1 2 3 5 8 13 21
 *  using while loop first taking temp and then assign to previous
 *  in between we update our current value with previous
+*/
+
+
+
+// Approch 2. Recursion
+class Solution {
+    public int climbStairs(int n) {
+       return steps(n);
+    }
+    public int steps(int n){
+        if(n<0){
+            return 0;
+        }
+        else if(n==0){
+            return 1;
+        }
+        return steps(n-1)+steps(n-2);
+    }
+}
+
+//Time Complexity O(2^n)
+//Space Complexity O(1)
+
+/*
+*
+* The time complexity of the recursive solution is exponential
+* a close upper bound is O(2^n). From each state 2,
 */
