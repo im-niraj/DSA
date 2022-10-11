ad0cbb47a6f72bc690f06d92666da126af002dfe
Niraj Kumar <64235951+im-niraj@users.noreply.github.com>
2022-10-11 21:47:35 +0530

    using TreeMap O(nLogn)

--- LeetCode_JAVA/136. Single Number.java
+++ LeetCode_JAVA/136. Single Number.java
@@ -17,3 +17,26 @@ class Solution {
 
 // this is brute force solution
 // Time Complexity = O(n^2)
+
+
+
+/*------------------------------------------*/
+class Solution {
+    public int singleNumber(int[] nums) {
+        
+        TreeMap<Integer,Integer> map = new TreeMap<>();
+        
+        for(int i=0; i<nums.length; i++){
+            map.put(nums[i], map.getOrDefault(nums[i], 0)+1);
+        }
+        for(Integer i : map.keySet()){
+            if(map.get(i) == 1){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
+// this is brute force solution
+// Time Complexity = O(nLogn)
+
