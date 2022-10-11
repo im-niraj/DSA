a8fa424fffef3578d0f74593cc46aa8c8ad66729
Niraj Kumar <64235951+im-niraj@users.noreply.github.com>
2022-10-11 22:09:03 +0530

    using math

--- LeetCode_JAVA/268. Missing Number.java
+++ LeetCode_JAVA/268. Missing Number.java
@@ -10,3 +10,17 @@ class Solution {
     }    
 }
 // time complexity: O(nlogn)
+
+/*-------------------------------------------*/
+class Solution {
+    public int missingNumber(int[] nums) {
+        int sum=0;
+        int n=nums.length;
+        for(int i=0; i<nums.length; i++){
+            sum+=nums[i];
+        }
+        return n*(n+1)/2-sum;
+    }
+}
+// time complexity: O(n)
+// Auxiliary space: O(1)
