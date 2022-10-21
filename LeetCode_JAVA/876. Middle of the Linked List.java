3b682ec278df3741220c1941cc8d0916f1143b24
Niraj Kumar <64235951+im-niraj@users.noreply.github.com>
2022-10-21 21:10:07 +0530

    Update 876. Middle of the Linked List.java

--- LeetCode_JAVA/876. Middle of the Linked List.java
+++ LeetCode_JAVA/876. Middle of the Linked List.java
@@ -9,9 +9,12 @@ class Solution {
         return slow;
     }
 }
+//  slow moves 1 fast moves 2, slow become 50 when fast become 100, basically, 2(fast) is twice the 1(slow) always
 
 
 
+
+/*------------------------------------------------------------------------------*/
 // Approch 2 by finding length of the nodes and traverse over half of the length
 
 class Solution {
