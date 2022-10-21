b7740e6bcde51ed49f5e94bc8459a6ae92d8a498
Niraj Kumar <64235951+im-niraj@users.noreply.github.com>
2022-10-21 21:05:39 +0530

    finding length and traverse half of the length

--- LeetCode_JAVA/876. Middle of the Linked List.java
+++ LeetCode_JAVA/876. Middle of the Linked List.java
@@ -9,3 +9,21 @@ class Solution {
         return slow;
     }
 }
+
+
+
+// Approch 2 by finding length of the nodes and traverse over half of the length
+
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        ListNode node = head;
+        int len=0;
+        while(node!=null){
+            len++;
+            node=node.next;
+        }
+        for(int i=0; i<len/2; i++)head=head.next;
+        
+        return head;
+    }
+}
