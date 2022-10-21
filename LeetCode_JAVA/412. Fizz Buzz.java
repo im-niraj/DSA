bf40ab16e2e640974ca9366ee07b41e76433778d
Niraj Kumar <64235951+im-niraj@users.noreply.github.com>
2022-10-21 20:24:57 +0530

    Using IntStream

--- LeetCode_JAVA/412. Fizz Buzz.java
+++ LeetCode_JAVA/412. Fizz Buzz.java
@@ -1,3 +1,18 @@
+// Approch 1
+class Solution {
+    public List<String> fizzBuzz(int n) {
+       return IntStream.rangeClosed(1, n).mapToObj(i -> {
+            if(i%3==0 && i%5==0) return "FizzBuzz";
+            else if(i%3==0) return "Fizz";
+            else if(i%5==0) return "Buzz";
+            else return String.valueOf(i);
+       }).collect(Collectors.toList());
+    }
+}
+
+
+/*-------------------------------------------*/
+// Approch 2
 class Solution {
     public List<String> fizzBuzz(int n) {
         List<String> list = new ArrayList<>();
