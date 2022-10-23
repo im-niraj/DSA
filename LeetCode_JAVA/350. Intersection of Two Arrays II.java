5d9246f948aa2689b1ed7c227aad3c7ad11c3a4b
Niraj Kumar <64235951+im-niraj@users.noreply.github.com>
2022-10-23 19:59:27 +0530

    brute force

---
+++ LeetCode_JAVA/350. Intersection of Two Arrays II.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int[] intersect(int[] nums1, int[] nums2) {
+       HashMap<Integer, Integer> map = new HashMap<>();
+       for(int i=0; i<nums1.length; i++){
+           map.put(nums1[i], map.getOrDefault(nums1[i], 0)+1);
+       }
+
+       ArrayList<Integer> list = new ArrayList<>();
+       for(int i=0; i<nums2.length; i++){
+           if(map.containsKey(nums2[i])){
+               if(map.get(nums2[i])>1){
+                   list.add(nums2[i]);
+                   map.put(nums2[i], map.get(nums2[i])-1);
+               }
+               else{
+                   list.add(nums2[i]);
+                   map.remove(nums2[i]);
+               }
+           }
+       }
+       int[] arr = new int[list.size()];
+       for(int i=0; i<list.size(); i++)arr[i] = list.get(i);
+       return arr;
+    }
+}
+
+// TC: O(n)
+// SC: O(n)
