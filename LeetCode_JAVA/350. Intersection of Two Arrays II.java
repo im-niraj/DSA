6aeb97f6c38fadd1e2e35a6a733a5d487b7360b2
Niraj Kumar <64235951+im-niraj@users.noreply.github.com>
2022-10-23 20:20:19 +0530

    similar to merge sort

--- LeetCode_JAVA/350. Intersection of Two Arrays II.java
+++ LeetCode_JAVA/350. Intersection of Two Arrays II.java
@@ -1,3 +1,33 @@
+// Approch 1
+class Solution {
+    public int[] intersect(int[] nums1, int[] nums2) {
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        
+        ArrayList<Integer> list = new ArrayList<>();
+        int n1=0; // index nums1 
+        int n2=0; // index nums2
+        
+        while(n1<nums1.length && n2<nums2.length){
+            if(nums1[n1]==nums2[n2]){
+                list.add(nums1[n1]);
+                n1++;
+                n2++;
+            }
+            else if (nums1[n1] < nums2[n2]) n1++;
+            else n2++;
+       }
+       
+       int[] arr = new int[list.size()];
+       for(int i=0; i<list.size(); i++)arr[i] = list.get(i);
+       return arr;
+    }
+}
+
+
+
+// -----------------------------------------------------
+// Approch 2
 class Solution {
     public int[] intersect(int[] nums1, int[] nums2) {
        HashMap<Integer, Integer> map = new HashMap<>();
