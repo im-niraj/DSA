e3bb465f9a4460b5251f74092f41982b3bf16462
Niraj Kumar <64235951+im-niraj@users.noreply.github.com>
2022-10-26 07:35:51 +0530

    O(n*m)

--- LeetCode_JAVA/566. Reshape the Matrix.java
+++ LeetCode_JAVA/566. Reshape the Matrix.java
@@ -1,3 +1,23 @@
+// Approch 1
+class Solution {
+    public int[][] matrixReshape(int[][] mat, int r, int c) {
+        if(r*c != mat.length * mat[0].length){
+            return mat;
+        }
+
+        int[][] res = new int[r][c];
+
+        for(int i=0; i < r*c; i++){
+            res[i/c][i%c] = mat[i / mat[0].length][i % mat[0].length];
+        }
+        return res;
+    }
+}
+
+// TC: O(n*m) linear
+// SC: O(n*m)
+
+// Approch 2
 class Solution {
     public int[][] matrixReshape(int[][] mat, int r, int c) {
         int elem1 = mat.length * mat[0].length;
