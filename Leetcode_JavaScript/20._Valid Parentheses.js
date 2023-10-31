f17cc6e793987b57c5d0cd6cbf52e5ec1a7dbeb3
Niraj Kumar <64235951+im-niraj@users.noreply.github.com>
2023-10-31 21:07:46 +0530

    Updated using Stack

--- Leetcode_JavaScript/20._Valid Parentheses.js
+++ Leetcode_JavaScript/20._Valid Parentheses.js
@@ -3,13 +3,23 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-    let parentheses = {
-        "(":")",
-        "{":"}",
-        "[":"]",
-    }
+    
+    let stack = [];
     for(let i=0; i<s.length; i++){
-        if(parentheses[s[i]])
+        if(s[i] === "(" || s[i] === "{" || s[i] === "["){
+            stack.push(s[i]);
+        }
+        else if(s[i] === ")" || s[i] === "}" || s[i] === "]"){
+            if(s[i] === ")" &&  stack[stack.length-1] === "("){
+                stack.pop();
+            }else if(s[i] === "}" &&  stack[stack.length-1] === "{"){
+                stack.pop();
+            }else if(s[i] === "]" &&  stack[stack.length-1] === "["){
+                stack.pop();
+            }else{
+                return false;
+            }
+        }
     }
-    return true;
+    return stack.length === 0;
 };
